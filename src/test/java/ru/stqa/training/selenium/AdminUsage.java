4fcaafb93a6cada742df860bf093d1538c8e21b6
Gavrilova <gavrilova.irina@gmail.com>
2017-01-28 10:10:23 -0600

    Created ApplicationManager.java class
    Replaced Inheritance with Delegation.

--- src/test/java/ru/stqa/training/selenium/AdminUsage.java
+++ src/test/java/ru/stqa/training/selenium/AdminUsage.java
@@ -8,23 +8,23 @@ public class AdminUsage extends TestBase {
 
   @Test
   public void testAdminUsage() {
-    gotoCatalogPage();
-    gotoManufacturesPage();
-    gotoAcmeCorpPage();
-    gotoCatalogPage();
-    gotoProductGroupPage();
-    openCatalogMenu();
-    initAddNewCategory();
-    fillCategoryForm(new CategoryData("Big Ducks"));
-    choosingProductFromCategories();
-    submitProductDuplicate();
-    initEditProduct();
-    fillProductForm(new ProductData("RD009", "RD009", "10.0000", "10", "Big Green Duck", "Best big green duck ever", "\\undefined"));
-    choosingProductInCategories();
-    submitDeleting();
-    choosingCategoryToEdit();
-    submitDeleting();
-    logoutAdminSession();
+    app.gotoCatalogPage();
+    app.gotoManufacturesPage();
+    app.gotoAcmeCorpPage();
+    app.gotoCatalogPage();
+    app.gotoProductGroupPage();
+    app.openCatalogMenu();
+    app.initAddNewCategory();
+    app.fillCategoryForm(new CategoryData("Big Ducks"));
+    app.choosingProductFromCategories();
+    app.submitProductDuplicate();
+    app.initEditProduct();
+    app.fillProductForm(new ProductData("RD009", "RD009", "10.0000", "10", "Big Green Duck", "Best big green duck ever", "\\undefined"));
+    app.choosingProductInCategories();
+    app.submitDeleting();
+    app.choosingCategoryToEdit();
+    app.submitDeleting();
+    app.logoutAdminSession();
   }
 
 }
