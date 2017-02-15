919ba5ac243f75c4bc04c364f26f38d7732b4650
Gavrilova <gavrilova.irina@gmail.com>
2017-02-14 18:43:41 -0600

    MyFirstTestSafari.java
    Get info about attributes of the text (color and location) in the Main
    page and Product pages.

--- src/test/java/ru/stqa/training/selenium/tests/MyFirstTestSafari.java
+++ src/test/java/ru/stqa/training/selenium/tests/MyFirstTestSafari.java
@@ -4,12 +4,12 @@ package ru.stqa.training.selenium.tests;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.safari.SafariDriver;
 import org.openqa.selenium.safari.SafariOptions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
+import static org.junit.Assert.assertEquals;
 
 
 /**
@@ -23,7 +23,7 @@ public class MyFirstTestSafari {
   @Before
   public void start() {
     SafariOptions options = new SafariOptions();
-    options.setUseCleanSession(true); //if you wish safari to forget session everytime
+    options.setUseCleanSession(true); //if you wish safari to forget session every time
     driver = new SafariDriver(options);
   }
 
@@ -31,18 +31,57 @@ public class MyFirstTestSafari {
   public void myFirstTest() throws InterruptedException {
     driver.get("http://localhost/litecart/en/");
     WebElement firstProduct = driver.findElement(By.cssSelector("div#box-campaigns li.product a.link"));
-    String productName = firstProduct.getText();
-    System.out.println("getText = " + productName);
-    System.out.println("getAttibute (href) = " + firstProduct.getAttribute("href"));
-    System.out.println("getAttibute (title) = " + firstProduct.getAttribute("title"));
+    String productText = firstProduct.getText();
+    System.out.println("getText = " + productText);
+    System.out.println("getAttribute (href) = " + firstProduct.getAttribute("href"));
+
+
+    String nameMainPage = firstProduct.getAttribute("title");
+    System.out.println("getAttribute (title) = " + nameMainPage);
+    String priceCampaignsMainPage = driver.findElement(By.cssSelector("div#box-campaigns strong.campaign-price")).getText();
+    System.out.println(priceCampaignsMainPage);
+
+
+    String priceRegularMainPage = driver.findElement(By.cssSelector("div#box-campaigns s.regular-price")).getText();
+
+    System.out.println(driver.findElement(By.cssSelector("div#box-campaigns s.regular-price")).getSize());
+
+
+    System.out.println(driver
+            .findElement(By.cssSelector("div#box-campaigns strong.campaign-price"))
+            .getText());
+    System.out.println(driver.
+            findElement(By.cssSelector("div#box-campaigns strong.campaign-price"))
+            .getSize());
+
+    Point location = driver.
+            findElement(By.cssSelector("div#box-campaigns strong.campaign-price")).getLocation();
+    Dimension size = driver.
+            findElement(By.cssSelector("div#box-campaigns strong.campaign-price")).getSize();
+
+    System.out.println("location = " + location + "; size = " + size);
+
 
     String urlFirstProduct = driver.getCurrentUrl();
     System.out.println(urlFirstProduct);
-    firstProduct.click();
-    //  sleep(5000);
-
-    new WebDriverWait(driver, 5).until((WebDriver dr1) -> dr1.findElement(By.cssSelector("h1.title")));
+    firstProduct.click();  //go to Product page
+    new WebDriverWait(driver, 5).until((WebDriver dr) -> dr.findElement(By.cssSelector("h1.title"))); //wait till h1.title will be present
     System.out.println(driver.getCurrentUrl());
+
+
+    WebElement productPage = driver.findElement(By.cssSelector("h1.title"));
+    System.out.println(productPage.getText());
+    assertEquals(productPage.getText(), nameMainPage); //assert the same names
+    assertEquals(driver.findElement(By.cssSelector("strong.campaign-price")).getText(), priceCampaignsMainPage);
+    assertEquals(driver.findElement(By.cssSelector("s.regular-price")).getText(), priceRegularMainPage);
+
+    String color = driver.findElement(By.cssSelector("strong.campaign-price")).getCssValue("color");
+    System.out.println("color = " + color);
+
+    // обычная цена серая и зачёркнутая, а акционная цена красная и жирная (это надо проверить на каждой странице независимо, при этом цвета на разных страницах могут не совпадать)
+
+//return to Main Page
+
 
     driver.get("http://localhost/litecart/en/");
 
@@ -53,20 +92,22 @@ public class MyFirstTestSafari {
     String productName2 = firstProduct2.getText();
     System.out.println("getText = " + productName2);
     System.out.println("getAttibute (href) = " + firstProduct2.getAttribute("href"));
+
     System.out.println("getAttibute (title) = " + firstProduct2.getAttribute("title"));
 
     String urlFirstProduct2 = driver.getCurrentUrl();
     System.out.println(urlFirstProduct2);
     firstProduct2.click();
-    //  sleep(5000);
 
-    new WebDriverWait(driver, 5).until((WebDriver dr1) -> dr1.findElement(By.cssSelector("h1.title")));
+
+    new WebDriverWait(driver, 5).until((WebDriver dr) -> dr.findElement(By.cssSelector("h1.title")));
     System.out.println(driver.getCurrentUrl());
   }
 
   @After
   public void stop() {
     driver.quit();
+    driver = null;
   }
 
 }
