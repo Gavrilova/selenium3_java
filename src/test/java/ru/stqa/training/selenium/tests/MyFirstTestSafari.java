1d41da1df60d2d83e0d2940bce12e6bf78a5782e
Gavrilova <gavrilova.irina@gmail.com>
2017-02-25 21:18:26 -0600

    Adding assertion of the same cssValue and same attributes of the prices
    In MyFirstTestSafari added:
    Assertion of the same cssValue and same attributes of the
    Campaign and Regular prices on the MainPage and ProductPage.

--- src/test/java/ru/stqa/training/selenium/tests/MyFirstTestSafari.java
+++ src/test/java/ru/stqa/training/selenium/tests/MyFirstTestSafari.java
@@ -2,6 +2,7 @@ package ru.stqa.training.selenium.tests;
 
 
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.*;
@@ -9,7 +10,11 @@ import org.openqa.selenium.safari.SafariDriver;
 import org.openqa.selenium.safari.SafariOptions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.lang.reflect.Array;
+
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 
 
 /**
@@ -30,78 +35,95 @@ public class MyFirstTestSafari {
   @Test
   public void myFirstTest() throws InterruptedException {
     driver.get("http://localhost/litecart/en/");
-    WebElement firstProduct = driver.findElement(By.cssSelector("div#box-campaigns li.product a.link"));
-    String productText = firstProduct.getText();
-    System.out.println("getText = " + productText);
-    System.out.println("getAttribute (href) = " + firstProduct.getAttribute("href"));
+   
+    WebElement firstProduct
+            = getFirstWebElementInCategory(By.cssSelector("div#box-campaigns li.product a.link"));
 
+    String nameMainPage = getProductAttribute(firstProduct, "title");
+    String productURL = getProductAttribute(firstProduct, "href");
+    String priceCampaignsMainPage = getProductPrice(firstProduct, By.cssSelector("strong.campaign-price"));
+    String priceRegularMainPage = getProductPrice(firstProduct, By.cssSelector("s.regular-price"));
 
-    String nameMainPage = firstProduct.getAttribute("title");
-    System.out.println("getAttribute (title) = " + nameMainPage);
-    String priceCampaignsMainPage = driver.findElement(By.cssSelector("div#box-campaigns strong.campaign-price")).getText();
-    System.out.println(priceCampaignsMainPage);
+    Point locationCampaign = getPoint(firstProduct, By.cssSelector("strong.campaign-price"));
+    Dimension sizeCampaign = getDimension(firstProduct, By.cssSelector("strong.campaign-price"));
+    String colorCampaigh = getValue(firstProduct, By.cssSelector("strong.campaign-price"), "color");
+    String fontCampaigh = getValue(firstProduct, By.cssSelector("strong.campaign-price"), "font-weight");
 
+    Point locationRegular = getPoint(firstProduct, By.cssSelector("s.regular-price"));
+    Dimension sizeRegular = getDimension(firstProduct, By.cssSelector("s.regular-price"));
+    String colorRegular = getValue(firstProduct, By.cssSelector("s.regular-price"), "color");
+    String textDecorationRegular = getValue(firstProduct, By.cssSelector("s.regular-price"), "text-decoration");
+    String fontRegular = getValue(firstProduct, By.cssSelector("s.regular-price"), "font-weight");
 
-    String priceRegularMainPage = driver.findElement(By.cssSelector("div#box-campaigns s.regular-price")).getText();
+   // System.out.println("Campaign price = " + priceCampaignsMainPage + "; location = " + locationCampaign +
+   //         "; size = " + sizeCampaign + "; color = " + colorCampaigh + fontCampaigh);
+   // System.out.println("Regular price  = " + priceRegularMainPage + "; location = " + locationRegular +
+   //         "; size = " + sizeRegular + "; color = " + colorRegular + textDecorationRegular + fontRegular);
 
-    System.out.println(driver.findElement(By.cssSelector("div#box-campaigns s.regular-price")).getSize());
+    attributeCheck(colorCampaigh, fontCampaigh, colorRegular, textDecorationRegular);
 
-
-    System.out.println(driver
-            .findElement(By.cssSelector("div#box-campaigns strong.campaign-price"))
-            .getText());
-    System.out.println(driver.
-            findElement(By.cssSelector("div#box-campaigns strong.campaign-price"))
-            .getSize());
-
-    Point location = driver.
-            findElement(By.cssSelector("div#box-campaigns strong.campaign-price")).getLocation();
-    Dimension size = driver.
-            findElement(By.cssSelector("div#box-campaigns strong.campaign-price")).getSize();
-
-    System.out.println("location = " + location + "; size = " + size);
 
 
     String urlFirstProduct = driver.getCurrentUrl();
     System.out.println(urlFirstProduct);
+
     firstProduct.click();  //go to Product page
     new WebDriverWait(driver, 5).until((WebDriver dr) -> dr.findElement(By.cssSelector("h1.title"))); //wait till h1.title will be present
     System.out.println(driver.getCurrentUrl());
 
-
     WebElement productPage = driver.findElement(By.cssSelector("h1.title"));
     System.out.println(productPage.getText());
     assertEquals(productPage.getText(), nameMainPage); //assert the same names
-    assertEquals(driver.findElement(By.cssSelector("strong.campaign-price")).getText(), priceCampaignsMainPage);
-    assertEquals(driver.findElement(By.cssSelector("s.regular-price")).getText(), priceRegularMainPage);
+    assertEquals(driver.findElement(By.cssSelector("strong.campaign-price")).getText(), priceCampaignsMainPage); //assert the same Campaign price
+    assertEquals(driver.findElement(By.cssSelector("s.regular-price")).getText(), priceRegularMainPage); //assert the same Regular price
 
-    String color = driver.findElement(By.cssSelector("strong.campaign-price")).getCssValue("color");
-    System.out.println("color = " + color);
+  }
 
-    // обычная цена серая и зачёркнутая, а акционная цена красная и жирная (это надо проверить на каждой странице независимо, при этом цвета на разных страницах могут не совпадать)
+  private void attributeCheck(String colorCampaigh, String fontCampaigh, String colorRegular, String textDecorationRegular) {
+    //verify that Campaign price is red and bold
+    //verify that Regular price is grey and line-through
 
-//return to Main Page
+    assertEquals(fontCampaigh, "bold");
+    assertEquals(textDecorationRegular, "line-through");
+
+    String[] redColorArray = {"rgb(204, 0, 0)", "#cc0000", "#c00"};
+    String[] greyColorArray = {"rgb(119, 119, 119)","#777777" , "#777"};
+
+    assertEquals(getAssert(redColorArray, colorCampaigh), true);
+    assertEquals(getAssert(greyColorArray, colorRegular), true);
+  }
+
+  private boolean getAssert(String[] array, String color) {
+    Boolean bool = false;
+    for (int i=0; i<array.length; i++) {
+      if (color.equals(array[i])) { bool = true;}
+    }
+    return bool;
+  }
+
+  private String getValue(WebElement firstProduct, By locator, String string) {
+    return firstProduct.findElement(locator).getCssValue(string);
+  }
 
 
-    driver.get("http://localhost/litecart/en/");
+  private Dimension getDimension(WebElement webElement, By locator) {
+    return webElement.findElement(locator).getSize();
+  }
 
-    System.out.println("refresh to http://localhost/litecart/en/");
-    System.out.println(driver.getCurrentUrl());
+  private Point getPoint(WebElement webElement, By locator) {
+    return webElement.findElement(locator).getLocation();
+  }
 
-    WebElement firstProduct2 = driver.findElement(By.cssSelector("li.product a.link"));
-    String productName2 = firstProduct2.getText();
-    System.out.println("getText = " + productName2);
-    System.out.println("getAttibute (href) = " + firstProduct2.getAttribute("href"));
+  private String getProductPrice(WebElement webElement, By locator) {
+    return webElement.findElement(locator).getText();
+  }
 
-    System.out.println("getAttibute (title) = " + firstProduct2.getAttribute("title"));
+  private String getProductAttribute(WebElement firstProduct, String string) {
+    return firstProduct.getAttribute(string);
+  }
 
-    String urlFirstProduct2 = driver.getCurrentUrl();
-    System.out.println(urlFirstProduct2);
-    firstProduct2.click();
-
-
-    new WebDriverWait(driver, 5).until((WebDriver dr) -> dr.findElement(By.cssSelector("h1.title")));
-    System.out.println(driver.getCurrentUrl());
+  private WebElement getFirstWebElementInCategory(By locator) {
+    return driver.findElement(locator);
   }
 
   @After
