4c2dc93ce27e34a3d1c21fedb9afbc4b616d549e
Gavrilova <gavrilova.irina@gmail.com>
2017-02-25 22:28:52 -0600

    Adding dimensionCheck comparison in pages
    In MyFirstTestSafari added:
    Dimension comparison for Campaign and Regular prices on the Main and
    Product pages.

--- src/test/java/ru/stqa/training/selenium/tests/MyFirstTestSafari.java
+++ src/test/java/ru/stqa/training/selenium/tests/MyFirstTestSafari.java
@@ -2,19 +2,17 @@ package ru.stqa.training.selenium.tests;
 
 
 import org.junit.After;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import org.openqa.selenium.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.safari.SafariDriver;
 import org.openqa.selenium.safari.SafariOptions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.lang.reflect.Array;
-
-import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
 
 
 /**
@@ -23,6 +21,8 @@ import static org.junit.Assert.assertFalse;
 
 public class MyFirstTestSafari {
 
+  public static final String campaignSelector = "strong.campaign-price";
+  public static final String regularSelector = "s.regular-price";
   private SafariDriver driver;
 
   @Before
@@ -35,49 +35,50 @@ public class MyFirstTestSafari {
   @Test
   public void myFirstTest() throws InterruptedException {
     driver.get("http://localhost/litecart/en/");
-   
+
     WebElement firstProduct
             = getFirstWebElementInCategory(By.cssSelector("div#box-campaigns li.product a.link"));
 
     String nameMainPage = getProductAttribute(firstProduct, "title");
-    String productURL = getProductAttribute(firstProduct, "href");
-    String priceCampaignsMainPage = getProductPrice(firstProduct, By.cssSelector("strong.campaign-price"));
-    String priceRegularMainPage = getProductPrice(firstProduct, By.cssSelector("s.regular-price"));
-
-    Point locationCampaign = getPoint(firstProduct, By.cssSelector("strong.campaign-price"));
-    Dimension sizeCampaign = getDimension(firstProduct, By.cssSelector("strong.campaign-price"));
-    String colorCampaigh = getValue(firstProduct, By.cssSelector("strong.campaign-price"), "color");
-    String fontCampaigh = getValue(firstProduct, By.cssSelector("strong.campaign-price"), "font-weight");
-
-    Point locationRegular = getPoint(firstProduct, By.cssSelector("s.regular-price"));
-    Dimension sizeRegular = getDimension(firstProduct, By.cssSelector("s.regular-price"));
-    String colorRegular = getValue(firstProduct, By.cssSelector("s.regular-price"), "color");
-    String textDecorationRegular = getValue(firstProduct, By.cssSelector("s.regular-price"), "text-decoration");
-    String fontRegular = getValue(firstProduct, By.cssSelector("s.regular-price"), "font-weight");
-
-   // System.out.println("Campaign price = " + priceCampaignsMainPage + "; location = " + locationCampaign +
-   //         "; size = " + sizeCampaign + "; color = " + colorCampaigh + fontCampaigh);
-   // System.out.println("Regular price  = " + priceRegularMainPage + "; location = " + locationRegular +
-   //         "; size = " + sizeRegular + "; color = " + colorRegular + textDecorationRegular + fontRegular);
+    String priceCampaignsMainPage = getProductPrice(firstProduct, By.cssSelector(campaignSelector));
+    String priceRegularMainPage = getProductPrice(firstProduct, By.cssSelector(regularSelector));
+    String colorCampaigh = getValue(firstProduct, By.cssSelector(campaignSelector), "color");
+    String fontCampaigh = getValue(firstProduct, By.cssSelector(campaignSelector), "font-weight");
+    String colorRegular = getValue(firstProduct, By.cssSelector(regularSelector), "color");
+    String textDecorationRegular = getValue(firstProduct, By.cssSelector(regularSelector), "text-decoration");
 
     attributeCheck(colorCampaigh, fontCampaigh, colorRegular, textDecorationRegular);
-
-
-
-    String urlFirstProduct = driver.getCurrentUrl();
-    System.out.println(urlFirstProduct);
+    dimentionCheck(firstProduct);
 
     firstProduct.click();  //go to Product page
     new WebDriverWait(driver, 5).until((WebDriver dr) -> dr.findElement(By.cssSelector("h1.title"))); //wait till h1.title will be present
-    System.out.println(driver.getCurrentUrl());
+    assertEquals(driver.findElement(By.cssSelector("h1.title")).getText(), nameMainPage); //assert the same names
 
-    WebElement productPage = driver.findElement(By.cssSelector("h1.title"));
-    System.out.println(productPage.getText());
-    assertEquals(productPage.getText(), nameMainPage); //assert the same names
-    assertEquals(driver.findElement(By.cssSelector("strong.campaign-price")).getText(), priceCampaignsMainPage); //assert the same Campaign price
-    assertEquals(driver.findElement(By.cssSelector("s.regular-price")).getText(), priceRegularMainPage); //assert the same Regular price
+    WebElement product = driver.findElement(By.cssSelector("div#box-product div.content"));
+    assertEquals(getProductPrice(product, By.cssSelector(campaignSelector)), priceCampaignsMainPage); //assert the same Campaign price
+    assertEquals(getProductPrice(product, By.cssSelector(regularSelector)), priceRegularMainPage); //assert the same Regular price
+    String colorCampaighProduct = getValue(product, By.cssSelector(campaignSelector), "color");
+    String colorRegularProduct = getValue(product, By.cssSelector(regularSelector), "color");
+    String fontCampaighProduct = getValue(product, By.cssSelector(campaignSelector), "font-weight");
+    String textDecorationRegularProduct = getValue(product, By.cssSelector(regularSelector), "text-decoration");
+    attributeCheck(colorCampaighProduct, fontCampaighProduct, colorRegularProduct, textDecorationRegularProduct);
+    dimentionCheck(product);
+
 
   }
+
+  private void dimentionCheck(WebElement webElement) {
+    double heightCampaign = getDimension(webElement, By.cssSelector(campaignSelector)).getHeight();
+    double widthCampaign = getDimension(webElement, By.cssSelector(campaignSelector)).getWidth();
+    double heightRegular = getDimension(webElement, By.cssSelector(regularSelector)).getHeight();
+    double widthRegular = getDimension(webElement, By.cssSelector(regularSelector)).getWidth();
+    assertEquals((getSquare(heightCampaign, widthCampaign) - getSquare(heightRegular, widthRegular) > 0), true);
+  }
+
+  private double getSquare(double height, double width) {
+    return (height * height + width * width);
+  }
+
 
   private void attributeCheck(String colorCampaigh, String fontCampaigh, String colorRegular, String textDecorationRegular) {
     //verify that Campaign price is red and bold
@@ -87,7 +88,7 @@ public class MyFirstTestSafari {
     assertEquals(textDecorationRegular, "line-through");
 
     String[] redColorArray = {"rgb(204, 0, 0)", "#cc0000", "#c00"};
-    String[] greyColorArray = {"rgb(119, 119, 119)","#777777" , "#777"};
+    String[] greyColorArray = {"rgb(119, 119, 119)", "#777777", "#777", "rgb(102, 102, 102)"};
 
     assertEquals(getAssert(redColorArray, colorCampaigh), true);
     assertEquals(getAssert(greyColorArray, colorRegular), true);
@@ -95,8 +96,10 @@ public class MyFirstTestSafari {
 
   private boolean getAssert(String[] array, String color) {
     Boolean bool = false;
-    for (int i=0; i<array.length; i++) {
-      if (color.equals(array[i])) { bool = true;}
+    for (int i = 0; i < array.length; i++) {
+      if (color.equals(array[i])) {
+        bool = true;
+      }
     }
     return bool;
   }
@@ -108,10 +111,6 @@ public class MyFirstTestSafari {
 
   private Dimension getDimension(WebElement webElement, By locator) {
     return webElement.findElement(locator).getSize();
-  }
-
-  private Point getPoint(WebElement webElement, By locator) {
-    return webElement.findElement(locator).getLocation();
   }
 
   private String getProductPrice(WebElement webElement, By locator) {
