1ac96e2267d3e750a18f095fc748c043e18e2947
Gavrilova <gavrilova.irina@gmail.com>
2017-03-30 20:24:01 -0500

    MathProblemSiliconValleyVoice.java class

--- src/test/java/ru/stqa/training/selenium/tests/MathProblemSiliconValleyVoice.java
+++ src/test/java/ru/stqa/training/selenium/tests/MathProblemSiliconValleyVoice.java
@@ -1,24 +1,23 @@
 package ru.stqa.training.selenium.tests;
 
 import org.testng.annotations.Test;
-
 import static java.util.Arrays.sort;
 
 /**
  * Created by irinagavrilova on 3/26/17.
  * Задача:
- * даны 8 неповторяющихся цифр, надо из составить из них два четырехзначных числа так,
+ * даны 8 неповторяющихся цифр, надо составить из них два четырехзначных числа так,
  * чтобы разница между ними была минимальна.
  */
 public class MathProblemSiliconValleyVoice {
 
   @Test
-  public void eigthNumbers1() {
-    int[] eightArray = {0, 1, 3, 4, 6, 7, 8, 5};
+  public void eigthNumbers() {
+    int[] eightArray = {0, 1, 3, 2, 6, 7, 8, 9};
     sort(eightArray);
     int[] arr = new int[6];
-    int number11 = 0;
-    int number22 = 0;
+    int numberMax = 0;
+    int numberMin = 0;
     int difference = Integer.MAX_VALUE;
 
     for (int i = 0; i < (eightArray.length - 1); i++) {
@@ -28,12 +27,12 @@ public class MathProblemSiliconValleyVoice {
         int number2 = (1000) * eightArray[i] + (100) * arr[5] + (10) * arr[4] + arr[3];
         if (difference > (number1 - number2)) {
           difference = (number1 - number2);
-          number11 = number1;
-          number22 = number2;
+          numberMax = number1;
+          numberMin = number2;
         }
       }
     }
-    System.out.println(number11 + "-" + number22 + " = " + (number11 - number22));
+    System.out.println(numberMax + "-" + numberMin + " = " + (numberMax - numberMin));
   }
 
   private void subArray(int[] eightArray, int[] arr, int i) {
