e1db4f1483b978aaeb8df7e690a1326f2a78ab77
Gavrilova <gavrilova.irina@gmail.com>
2017-04-21 10:04:01 -0500

    Added @FindBy annotations to ProductPageHelper.java;

--- src/test/java/ru/stqa/training/selenium/appmanager/ProductPageHelper.java
+++ src/test/java/ru/stqa/training/selenium/appmanager/ProductPageHelper.java
@@ -2,6 +2,9 @@ package ru.stqa.training.selenium.appmanager;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -19,25 +22,50 @@ public class ProductPageHelper extends HelperBase {
 
   public ProductPageHelper(WebDriver driver) {
     super(driver);
+    PageFactory.initElements(driver, this);
   }
 
   WebDriverWait wait = new WebDriverWait(driver, 10);
 
+  @FindBy(css = "span.quantity")
+  WebElement cartQuantity;
+
+  @FindBy(css = "select option")
+  List<WebElement> options;
+
+  @FindBy(css = "td.quantity button")
+  WebElement addToCartButton;
+
+  @FindBy(css = "select")
+  WebElement selectElement;
+
+  @FindBy(css = "select")
+  List<WebElement> selectList;
+
+  @FindBy(css = "div#cart a.link")
+  WebElement checkOut;
+
+  @FindBy(css = "table.dataTable")
+  WebElement form;
+
+  @FindBy(css = "em")
+  WebElement emptyMessage;
+
   public void addToCart() throws InterruptedException {
     int cartQuantity = cartQuantity();
-    new WebDriverWait(driver, 20).until((WebDriver dr) -> dr.findElement(By.cssSelector("td.quantity button")));
+    new WebDriverWait(driver, 20).until((WebDriver dr) -> addToCartButton);
     if (isElementPresent(By.cssSelector("select"))) {
-      new WebDriverWait(driver, 20).until((WebDriver dr) -> dr.findElements(By.cssSelector("select option")).get(1));
-      new Select(driver.findElement(By.cssSelector("select"))).selectByValue(selectOptions());
+      new WebDriverWait(driver, 20).until((WebDriver dr) -> options.get(1));
+      new Select(selectElement).selectByValue(selectOptions());
     }
-    driver.findElement(By.cssSelector("td.quantity button")).click();
+    addToCartButton.click();
     wait.until(ExpectedConditions.textToBe(By.cssSelector("span.quantity"), String.valueOf(cartQuantity + 1)));
   }
 
 
   private String selectOptions() {
     Random rnd = new Random();
-    List<String> list = driver.findElements(By.cssSelector("select option"))
+    List<String> list = options
             .stream().map((d) -> d.getAttribute("value"))
             .collect(Collectors.toList());
     int index = 1 + rnd.nextInt(list.size() - 1);
@@ -46,15 +74,17 @@ public class ProductPageHelper extends HelperBase {
 
   public void checkOut() {
     int quantity = cartQuantity();
-    driver.findElement(By.cssSelector("div#cart a.link")).click();
+    checkOut.click();
     if (quantity != 0) {
-      new WebDriverWait(driver, 20).until((WebDriver dr) -> dr.findElement(By.cssSelector("table.dataTable")));
+      new WebDriverWait(driver, 20).until((WebDriver dr) -> checkOut);
     } else {
-      new WebDriverWait(driver, 20).until((WebDriver dr) -> dr.findElement(By.cssSelector("em")));
+      new WebDriverWait(driver, 20).until((WebDriver dr) -> emptyMessage);
     }
   }
 
+
   private int cartQuantity() {
-    return valueOf(driver.findElement(By.cssSelector("span.quantity")).getText());
+    return valueOf(cartQuantity.getText());
   }
+
 }
